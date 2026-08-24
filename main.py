93495f506eaf2c23c4d2e85e46e7e9e37fd2d30c
vocasgjnvv <pranks3455@gmail.com>
2026-08-24 17:24:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 
 
 logging.basicConfig(level=logging.INFO)
