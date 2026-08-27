fac1d04ccd1038602fc0c6b3d8f7d765f69575b9
vocasgjnvv <pranks3455@gmail.com>
2026-08-27 14:23:51 +0200

    Update quest_service.py

--- quest_service.py
+++ quest_service.py
@@ -52,6 +52,9 @@ class QuestService:
         mission_id: int,
     ):
         return self.db.get_mission(mission_id)
+        
+    def seed_missions(self, missions):
+        self.db.seed_missions(missions)
 
     def get_all_missions(self):
         return self.db.get_all_missions()
