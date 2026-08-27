e79e4500749a81ca99383ffe877b1ed9330ccf31
vocasgjnvv <pranks3455@gmail.com>
2026-08-27 16:50:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -64,6 +64,7 @@ admin_menu = ReplyKeyboardMarkup(
     ],
     resize_keyboard=True,
 )
+
 admin_panel_menu = ReplyKeyboardMarkup(
     keyboard=[
         [
@@ -84,26 +85,7 @@ admin_panel_menu = ReplyKeyboardMarkup(
     resize_keyboard=True,
 )
 
-admin_menu = ReplyKeyboardMarkup(
-    keyboard=[
-        [
-            KeyboardButton(text="🕵️ Начать"),
-            KeyboardButton(text="🎯 Миссии"),
-        ],
-        [
-            KeyboardButton(text="👤 Мой профиль"),
-            KeyboardButton(text="🏆 Рейтинг"),
-        ],
-        [
-            KeyboardButton(text="📜 Правила"),
-            KeyboardButton(text="💬 Чат"),
-        ],
-        [
-            KeyboardButton(text="👑 Админ-панель"),
-        ],
-    ],
-    resize_keyboard=True,
-)
+
 def init_db():
     connection = sqlite3.connect(DB_FILE)
 
